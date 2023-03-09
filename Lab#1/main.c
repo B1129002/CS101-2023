0f189bb9cd4cbce87bbfa33a4aca68fa04e0d42c
B1129002 <127376851+B1129002@users.noreply.github.com>
2023-03-09 14:13:45 +0800

    Update main.c

--- Lab#1/main.c
+++ Lab#1/main.c
@@ -5,20 +5,12 @@ int main(){
     FILE* fp;
     int arr_write[5] = {1, 2, 3, 4, 5};
     int arr_read[5];
-    
-    if((fp = fopen("a.bin", "wb+")) == NULL) {
-        printf("Cannot open the file...");
-        exit(1);
+    fp = fopen("a.bin","wb+");
+    fwrite(arr_write,sizeof(int),5,fp);
+    fseek(fp,0,SEEK_SET);
+    fread(arr_read,sizeof(arr_read),5,fp);
+    for(int i=0;i<5;i++){
+        printf("%d",arr_read[i]);
     }
-    if(fwrite(arr_write, sizeof(arr_write), 1, fp) != 1){
-        printf("File write error....\n");
-    }
-    fseek(fp, 0, SEEK_SET);
-    
-    if(fread(arr_read, sizeof(arr_read), 1, fp) != 1){
-        printf("File read error....\n");
-    }
-    
-
     return 0;
 }
